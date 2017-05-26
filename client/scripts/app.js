e07403d4c70d49520ffc0b73e7c4466f2457c40e
dandersonstack <danieltanderson@berkeley.edu>
2017-05-26 12:33:16 -0700

    first few tests are passing, created new empty app.js

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,44 @@
 // YOUR CODE HERE:
+class App{
+  constructor() {
+    this.title = "some title";
+  }
+  init() {
+    //starts something here
+  }
+  send(message) {
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: 'http://parse.CAMPUS.hackreactor.com/chatterbox/classes/messages',
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        //handle escaping out the message to prevent from xss it should happen here
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+  }
+  fetch(){
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: 'http://parse.CAMPUS.hackreactor.com/chatterbox/classes/messages',
+      type: 'GET',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        //Supply the data request back
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+  }
+};
+var app = new App();
