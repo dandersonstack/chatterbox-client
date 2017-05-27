5d65818923d130ba1371c0616e0f8a7601cd1b1e
Hack Reactor Student <student@hackreactor.com>
2017-05-26 17:50:37 -0700

    adding in changes for messages, rooms, and deleting messages to pass all initial tests

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,15 +1,17 @@
 
 class App {
   constructor() {
-    this.title = 'some title';
+    this.server = 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages';
+
   }
   init() {
     //starts something here
   }
+
   send(message) {
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
-      url: 'http://parse.CAMPUS.hackreactor.com/chatterbox/classes/messages',
+      url: this.server,
       type: 'POST',
       data: JSON.stringify(message),
       contentType: 'application/json',
@@ -23,10 +25,11 @@ class App {
       }
     });
   }
+
   fetch() {
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
-      url: 'http://parse.CAMPUS.hackreactor.com/chatterbox/classes/messages',
+      url: this.server,
       type: 'GET',
       data: JSON.stringify(message),
       contentType: 'application/json',
@@ -40,7 +43,43 @@ class App {
       }
     });
   }
+  clearMessages() {
+    let $messages = $(`<div id='chats'></div>`);
+    var node = document.getElementById('chats');
+    node.parentElement.removeChild(document.getElementById('chats'));
+    document.body.appendChild($messages[0]);
+  }
+
+  renderRoom(room) {
+    let $roomTemplate = $(`<option value="${room}">${room}</option>`);
+    document.getElementById('roomSelect').appendChild($roomTemplate[0]);
+  }
+
+  makeMessageInstance(userName, messageText) { // TO DO:
+  }
+
+  renderMessage(message) {
+// TO DO: USE THE 'escape.js' SCRIPT BEFOREHAND
+    let $messageElement = $(
+      `<div class="message">
+         <h3 class="messageUser"> ${message.username} </h3>
+         <p class="messageText"> ${message.text} </p>
+       </div>}`);
+
+    document.getElementById('chats').appendChild($messageElement[0]);
+  }
+
+  _renderMessages(AllTheMessages) {
+  // INIT VARIABLE TO REFER TO DESIRED DOM NODE
+  // INIT MESSAGE ELEMENT TEMPLATE
+    // ITERATE OVER MESSAGE ARRAY (WHICH WE RECEIVED FROM THE SERVER)
+      // APPEND EACH ELEMENT TO CORRECT DOM NODE (WRAPPED IN THE MESSAGE ELEMENT TEMPLATE)
+  }
+
 }
+
+
+
 var app = new App();
 
 // - COMPLETE 'FETCH' METHOD...
@@ -48,22 +87,3 @@ var app = new App();
 // - UNDERSTAND HOW THOSE ELEMENTS ARE TO BE REPEATED
 
 // -
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- spec/chatterboxSpec.js
+++ spec/chatterboxSpec.js
@@ -58,9 +58,11 @@ describe('chatterbox', function() {
       });
 
       it('should submit a GET request via $.ajax', function(done) {
+
         app.fetch();
         expect($.ajax.calledOnce).to.be.true;
         ajaxUrl = typeof $.ajax.args[0][0] === 'string' ? $.ajax.args[0][0] : $.ajax.args[0][0].url;
+
         expect(ajaxUrl).to.equal(app.server);
         done();
       });
@@ -80,7 +82,7 @@ describe('chatterbox', function() {
           text: 'Never underestimate the power of the Schwartz!',
           roomname: 'lobby'
         };
-
+        expect($('#chats').children().length).to.equal(0);
         app.renderMessage(message);
 
         expect($('#chats').children().length).to.equal(1);
